0827b0d3df135ed63545e525e3b3b2530985d7ae
bhuvana1990 <maragani.bhuvana@gmail.com>
2018-05-21 06:35:29 +0400

    added new class message in test class

--- testProject/src/com/icicibank/loans/carloans/TextClass.java
+++ testProject/src/com/icicibank/loans/carloans/TextClass.java
@@ -5,6 +5,7 @@ public class TextClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a test class");
+		System.out.println("this is a test class 2");
 	}
 
 }
