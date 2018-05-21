38e2614c7acfa433d23eb52d67365dbc6b3bfc14
bhuvana1990 <maragani.bhuvana@gmail.com>
2018-05-21 06:27:31 +0400

    Revert "updated test class in car loans"
    This reverts commit caf5c05fefce6caa40dc01980729cf893b90c38b.

--- testProject/src/com/icicibank/loans/carloans/TextClass.java
+++ testProject/src/com/icicibank/loans/carloans/TextClass.java
@@ -5,8 +5,6 @@ public class TextClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a test class");
-		System.out.println("test class 2");
 	}
-	
 
 }
