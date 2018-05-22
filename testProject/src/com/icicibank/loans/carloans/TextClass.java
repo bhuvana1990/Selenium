5506b2e359b00fd43f74db26bc06811eda506441
bhuvana1990 <maragani.bhuvana@gmail.com>
2018-05-22 16:48:09 +0400

    Revert "spaces deleted text"
    This reverts commit 3d293839997c5576677111d21b93c4e26f6cad30.

--- testProject/src/com/icicibank/loans/carloans/TextClass.java
+++ testProject/src/com/icicibank/loans/carloans/TextClass.java
@@ -6,7 +6,9 @@ public class TextClass {
 		// TODO Auto-generated method stub
 		System.out.println("this is a test class");
 		System.out.println("this is a test class 2");
-		System.out.println("spaces deleted");
+		
+		
+		   
 	}
 
 }
